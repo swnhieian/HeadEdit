51ad78f9a2b22b5de0666d783e92bdb4d76d0bc9
Jxu-Bupt <Jxu@bupt.edu.cn>
2017-09-12 11:09:40 +0800

    log structure

--- HeadEdit/Log.cs
+++ HeadEdit/Log.cs
@@ -1,14 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HeadEdit
 {
-    class Log
+    public enum LogType { NextTask, TaskStart, MouseDown, Type, Delete, Enter, Select,  F1, Shift, Esc };
+    public class Log
     {
-        public enum LogType { NextTask, TaskStart, MouseDown, Type, Delete, Enter, Select,  F1, Shift, Esc };
+        private MainWindow window;
+        private List<LogRecord> loglist;
+        private StreamWriter sw;
+        private DateTime startTime;
+        private string logDir;
+        public Log(MainWindow mainWindow ,DateTime time )
+        {
+            this.window = mainWindow;
+            this.startTime = time;
+
+            logDir = Directory.GetCurrentDirectory() + "\\logs\\" ;
+        }
+
+        public void addLog(LogType type, DateTime time)
+        {
+            loglist.Add(new LogRecord(type, time));
+        }
+
+        public void addLog(LogType type, DateTime time, string wrongWord, string targetWord)
+        {
+            loglist.Add(new LogRecord(type, time, wrongWord, targetWord));
+        }
+
+        public void saveLog()
+        {
+            if (loglist == null) return;
+            string fileName = startTime.ToString("_MM_dd_HH_mm_ss")+ ".txt";
+
+            sw = new StreamWriter(logDir + "\\" + fileName, true);
+            foreach (LogRecord log in loglist)
+            {
+                    sw.WriteLine(log.ToString());
+            }
+            sw.Close();
+            loglist.Clear();
+        }
 
     }
 }
---
+++ HeadEdit/Logrecord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace HeadEdit
+{
+    class LogRecord
+    {
+        LogType type;
+        DateTime time;
+        string wrongWord;
+        string targetWord;
+
+        public LogRecord(LogType type ,DateTime time)
+        {
+            this.type = type;
+            this.time = time;
+        }
+
+        public override string ToString()
+        {
+            string str;
+            if (type == LogType.NextTask)//当进行下一个任务时，加入任务的相关信息
+            {
+                str = (type.ToString() + "," + time.ToString() + "," + wrongWord + "," + targetWord);
+            }
+            else
+            {
+                str = (type.ToString() + "," + time.ToString() );
+            }
+            return str;
+        }
+    }
+}
